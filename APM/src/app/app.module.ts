806f802404bf3714210c65af7d14b7837d064ae8
james olamide <horlamidex1@gmail.com>
2020-07-09 12:31:05 +0100

    First commit

---
+++ APM/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+
+import { ConvertToSpacesPipe } from './shared/convert-to-space.pipe';
+import { StarComponent } from './shared/star.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductsComponent,
+    ConvertToSpacesPipe,
+    StarComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
